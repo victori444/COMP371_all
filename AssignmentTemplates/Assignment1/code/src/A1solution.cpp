0bd6a1348f4f2a6e4d97f394f07bb6ca9646d3c3
victori444 <victoriatthoang@gmail.com>
2026-02-22 11:13:44 -0500

    [A1] Working Phong & Flat Shader

--- AssignmentTemplates/Assignment1/code/src/A1solution.cpp
+++ AssignmentTemplates/Assignment1/code/src/A1solution.cpp
@@ -3,85 +3,145 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
-// IMPORTANT 1
 #include <GL/glew.h>    // Include GLEW - OpenGL Extension Wrangler
-#include <GLFW/glfw3.h> // GLFW provides a cross-platform interface for creating a graphical context,
-                        // initializing OpenGL and binding inputs
+#include <GLFW/glfw3.h> // GLFW provides a cross-platform interface for creating a graphical context, initializing OpenGL and binding inputs
 #include <glm/glm.hpp>  // GLM is an optimized math library with syntax to similar to OpenGL Shading Language
 using namespace std;
+
+A1solution::A1solution() {}
+
+// =============================== //
+//           STRUCTURES            //
+// =============================== //
+
+struct SceneData {
+    glm::mat4 modelViewMatrix;
+    glm::mat4 projectionMatrix;
+    int width;
+    int height;
+    std::vector<glm::vec3> vertices;
+    std::vector<glm::vec3> normals;
+    std::vector<unsigned int> indices;
+};
+
+struct Mesh {
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int EBO;
+    size_t indexCount;
+};
+
+struct ShaderPrograms {
+    unsigned int phongShader;
+    unsigned int flatShader;
+    unsigned int circleShader;
+    unsigned int voronoiShader;
+};
+
+// =============================== //
+//       HELPER DECLARATIONS       //
+// =============================== //
+
+// from capsule 2
+// const char* getVertexShaderSource();
+// const char* getFragmentShaderSource();
+// int compileAndLinkShaders(const char* vertexShaderSource, const char* fragmentShaderSource);
+
+// // run() broken down
+// SceneData loadSceneFromFile(const std::string& filename);
+// GLFWwindow* initOpenGL(int width, int height);
+// ShaderPrograms initShaders();
+// Mesh createMesh(const std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& normals, const std::vector<unsigned int>& indices);
+// void setMatrices(unsigned int shaderProgram, const glm::mat4& modelViewMatrix, const glm::mat4& projectionMatrix);
+// void mainLoop(GLFWwindow* window, ShaderPrograms& shaders, Mesh& mesh, const SceneData& scene);
+// void handleInput(GLFWwindow& window, unsigned int& activeShader, ShaderPrograms& shaders);
+
 
 // =============================== //
 //        HELPER FUNCTIONS         //
 // =============================== //
-const char* getVertexShaderSource();
-const char* getFragmentShaderSource();
-int compileAndLinkShaders(const char* vertexShaderSource, const char* fragmentShaderSource);
 
+std::vector<glm::vec3> calculateNormals(const std::vector<glm::vec3>& vertices, const std::vector<unsigned int>& indices) {
+    
+    std::vector<glm::vec3> normals(vertices.size(), glm::vec3(0.0f));
+
+    for (size_t i = 0; i < indices.size(); i += 3) {
+        int i0 = indices[i];
+        int i1 = indices[i+1];
+        int i2 = indices[i+2];
+        glm::vec3 v0 = vertices[i0];
+        glm::vec3 v1 = vertices[i1];
+        glm::vec3 v2 = vertices[i2];
+        glm::vec3 edge1 = v1 - v0;
+        glm::vec3 edge2 = v2 - v0;
+
+        glm::vec3 faceNormal = glm::normalize(glm::cross(edge1, edge2));
+        normals[i0] += faceNormal;
+        normals[i1] += faceNormal;
+        normals[i2] += faceNormal;
+    }
+
+    // normalize accumulated normals
+    for (size_t i = 0; i < normals.size(); ++i) {
+        normals[i] = glm::normalize(normals[i]);
+    }
+
+    return normals;
+}
 
 // =============================== //
-//           CONSTRUCTOR           //
+//       MAIN FUNCTION BLOCKS      //
 // =============================== //
-A1solution::A1solution() {}
 
-// =============================== //
-//            MAIN RUN             //
-// =============================== //
-void A1solution::run(const std::string& filename) {
+SceneData loadSceneFromFile(const std::string& filename) {
 
-    // =============================== //
-    //          READ FROM FILE         //
-    // =============================== //
+    SceneData scene;
 
     // Open input file
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Can not open file\n";
-        return;
+        exit(EXIT_FAILURE);
     }
 
     // Modelview matrix
-    glm::mat4 modelViewMatrix;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            file >> modelViewMatrix[i][j];
+            file >> scene.modelViewMatrix[i][j];
         }
     }
-
     // Projection matrix
-    glm::mat4 projectionMatrix;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            file >> projectionMatrix[i][j];
+            file >> scene.projectionMatrix[i][j];
         }
     }
 
     // window height & width
-    int width;
-    int height;
-    file >> width >> height;
+    file >> scene.width >> scene.height;
 
-    // Geometry
+    // Geometry: vertices, indices, normals
     int N;
     file >> N;
-
-    std::vector<glm::vec3> vertices(N);
+    scene.vertices.resize(N);
     for (int i = 0; i < N; i++) {
-        file >> vertices[i].x >> vertices[i].y >> vertices[i].z;
+        file >> scene.vertices[i].x >> scene.vertices[i].y >> scene.vertices[i].z;
     }
 
     int M;
     file >> M;
-
-    std::vector<unsigned int> indices(M * 3);
+    scene.indices.resize(M * 3);
     for (int i = 0; i < M * 3; i++) {
-        file >> indices[i];
+        file >> scene.indices[i];
     }
-    file.close();
 
-    // =============================== //
-    //          OPENGL WINDOW          //
-    // =============================== //
+    scene.normals = calculateNormals(scene.vertices, scene.indices);
+
+    file.close();
+    return scene;
+}
+
+GLFWwindow* initOpenGL(int width, int height) {
 
     // Initialize GLFW and OpenGL version
     glfwInit();
@@ -96,7 +156,7 @@ void A1solution::run(const std::string& filename) {
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return;
+        return nullptr;
     }
     glfwMakeContextCurrent(window);
 
@@ -105,174 +165,51 @@ void A1solution::run(const std::string& filename) {
     if (glewInit() != GLEW_OK) {
         std::cerr << "Failed to create GLEW" << std::endl;
         glfwTerminate();
-        return;
+        return nullptr;
     }
     glViewport(0, 0, width, height);
     glEnable(GL_DEPTH_TEST);
 
-    // Black background
-    // glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    return window;
+}
 
-    // Compile and link shaders
-    int vertexColorProgram = compileAndLinkShaders(getVertexShaderSource(), getFragmentShaderSource());
-    // int voronoiProgram = compileAndLinkShaders(getVertexShaderSource2(), getFragmentShaderSource2());
+Mesh createMesh(
+    const std::vector<glm::vec3>& vertices, 
+    const std::vector<glm::vec3>& normals,
+    const std::vector<unsigned int>& indices
+) {
+    
+    Mesh mesh;
+    mesh.indexCount = indices.size();
 
-    int shaderProgram = vertexColorProgram;
-    // int shaderProgram = voronoiProgram;
-
-    // debug_gl(0);
-
-    // =============================== //
-    //         UPLOAD GEO -> GPU       //
-    // =============================== //
-    // Render triangle
-    unsigned int VAO, VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
+    glGenVertexArrays(1, &mesh.VAO);
+    glBindVertexArray(mesh.VAO);
 
     // Vertex buffer
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glGenBuffers(1, &mesh.VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, mesh.VBO);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // Normals buffer
+    unsigned int normalBuffer;
+    glGenBuffers(1, &normalBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
+    glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), normals.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+    glEnableVertexAttribArray(1);
+
     // Index buffer
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glGenBuffers(1, &mesh.EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
     glBindVertexArray(0);
 
-    // =============================== //
-    //           PASS MATRICES         //
-    // =============================== //
-    
-    glUseProgram(shaderProgram);
-    GLint modelLoc = glGetUniformLocation(shaderProgram, "modelViewMatrix");
-    GLint projLoc = glGetUniformLocation(shaderProgram, "projectionMatrix");
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &modelViewMatrix[0][0]);
-    glUniformMatrix4fv(projLoc, 1, GL_FALSE, &projectionMatrix[0][0]);
-
-    // =============================== //
-    //            MAIN LOOP            //
-    // =============================== //
-    while(!glfwWindowShouldClose(window))
-    {
-        // Each frame, reset color of each pixel to glClearColor
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-
-    //     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-        glfwSwapBuffers(window);
-
-        // Detect inputs
-        glfwPollEvents();
-
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-            glfwSetWindowShouldClose(window, true);
-        }
-
-    //     if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS) {
-    //         shaderProgram = vertexColorProgram;
-    //     }
-
-    //     if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS) {
-    //         shaderProgram = voronoiProgram;
-    //     }
-    }
-
-    // Shutdown GLFW
-    glfwTerminate();
-
-    return;
-
+    return mesh;
 }
 
-// =============================== //
-//        HELPER FUNCTIONS         //
-// =============================== //
-
-const char* getVertexShaderSource()
-{
-    return
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "uniform mat4 modelViewMatrix;\n"
-    "uniform mat4 projectionMatrix;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = projectionMatrix * modelViewMatrix * vec4(aPos, 1.0);\n"
-    "}\n";
-}
-
-const char* getFragmentShaderSource()
-{
-    return
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0, 0.5, 0.5, 1.0);\n"
-    "}\n";
-}
-
-// const char* getVertexShaderSource2()
-// {
-//     return
-//     "#version 330 core\n"
-//     "layout (location = 0) in vec2 aPos;"
-//     "layout (location = 1) in vec3 col;"
-//     "layout (location = 2) in vec2 p1;"
-//     "layout (location = 3) in vec2 p2;"
-//     "layout (location = 4) in vec2 p3;"
-//     "out vec3 vertexColor;"
-//     "out vec2 op1;"
-//     "out vec2 op2;"
-//     "out vec2 op3;"
-//     "out vec2 op;"
-//     ""
-//     "void main()"
-//     "{"
-//     "   gl_Position = vec4(aPos, 0, 1);"
-//     "   vertexColor = col;"
-//     "   op1 = p1;"
-//     "   op2 = p2;"
-//     "   op3 = p3;"
-//     "   op = aPos;"
-//     "}";
-// }
-
-// const char* getFragmentShaderSource2()
-// {
-//     return
-//     "#version 330 core\n"
-//     "in vec3 vertexColor;"
-//     "in vec2 op1;"
-//     "in vec2 op2;"
-//     "in vec2 op3;"
-//     "in vec2 op;"
-//     "out vec3 FragColor;"
-//     ""
-//     "void main()"
-//     "{"
-//     "    if (distance(op, op1) <= distance(op, op2)"
-//     "        && distance(op, op1) <= distance(op, op3)) {"
-//     ""
-//     "        FragColor = vec3(1, 0, 0);"
-//     ""
-//     "    } else if (distance(op, op2) <= distance(op, op3)) {"
-//     ""
-//     "        FragColor = vec3(0, 1, 0);"
-//     "    } else {"
-//     "        FragColor = vec3(0, 0, 1);"
-//     "    }"
-//     "}";
-// }
 
 int compileAndLinkShaders(const char* vertexShaderSource, const char* fragmentShaderSource)
 {
@@ -321,6 +258,9 @@ int compileAndLinkShaders(const char* vertexShaderSource, const char* fragmentSh
         std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
 
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     return shaderProgram;
 }
 
@@ -328,7 +268,6 @@ void createRenderingData(unsigned int& VAO, unsigned int& VBO,unsigned int& CBO,
 {
 
     // Define and upload geometry to the GPU here ...
-
     float vertices[] = {
         -0.5f, -0.5f,
         0.5f,  -0.5f,
@@ -398,6 +337,219 @@ void createRenderingData(unsigned int& VAO, unsigned int& VBO,unsigned int& CBO,
     glBindVertexArray(0);
 }
 
+// =============================== //
+// ========== SHADERS ============ //
+// =============================== //
 
+const char* getVertexShaderSource()
+{
+    return
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "uniform mat4 modelViewMatrix;\n"
+    "uniform mat4 projectionMatrix;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = projectionMatrix * modelViewMatrix * vec4(aPos, 1.0);\n"
+    "}\n";
+}
 
+const char* getFragmentShaderSource()
+{
+    return
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0, 0.5, 0.5, 1.0);\n"
+    "}\n";
+}
 
+// =============================== //
+//             PHONG               //
+// =============================== //
+
+const char* getPhongVertexShader() {
+    return
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aNormal;\n"
+
+    "uniform mat4 modelViewMatrix;\n"
+    "uniform mat4 projectionMatrix;\n"
+
+    "out vec3 FragPos;\n"
+    "out vec3 Normal;\n"
+
+    "void main()\n"
+    "{\n"
+    "   FragPos = vec3(modelViewMatrix * vec4(aPos, 1.0));\n"
+    "   Normal = mat3(transpose(inverse(modelViewMatrix))) * aNormal;\n"
+    "   gl_Position = projectionMatrix * vec4(FragPos, 1.0);\n"
+    "}\n";
+}
+
+const char* getPhongFragmentShader()
+{
+    return
+    "#version 330 core\n"
+    "in vec3 FragPos;\n"
+    "in vec3 Normal;\n"
+
+    "out vec4 FragColor;\n"
+
+    "uniform vec3 lightPos;\n"
+    "uniform vec3 viewPos;\n"
+    "uniform vec3 lightColor;\n"
+    "uniform vec3 objectColor;\n"
+
+    "void main()\n"
+    "{\n"
+    "   float ambientStrength = 0.2;\n"
+    "   vec3 ambient = ambientStrength * lightColor;\n"
+
+    "   vec3 norm = normalize(Normal);\n"
+    "   vec3 lightDir = normalize(lightPos - FragPos);\n"
+    "   float diff = max(dot(norm, lightDir), 0.0);\n"
+    "   vec3 diffuse = diff * lightColor;\n"
+
+    "   float specularStrength = 0.5;\n"
+    "   vec3 viewDir = normalize(viewPos - FragPos);\n"
+    "   vec3 reflectDir = reflect(-lightDir, norm);\n"
+    "   float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);\n"
+    "   vec3 specular = specularStrength * spec * lightColor;\n"
+
+    "   vec3 result = (ambient + diffuse + specular) * objectColor;\n"
+    "   FragColor = vec4(result, 1.0);\n"
+    "}\n";
+}
+
+// =============================== //
+//             FLAT                //
+// =============================== //
+
+const char* getFlatVertexShader() {
+    return
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+
+    "uniform mat4 modelViewMatrix;\n"
+    "uniform mat4 projectionMatrix;\n"
+
+    "out vec3 FragPos;\n"
+
+    "void main()\n"
+    "{\n"
+    "   FragPos = vec3(modelViewMatrix * vec4(aPos, 1.0));\n"
+    "   gl_Position = projectionMatrix * vec4(FragPos, 1.0);\n"
+    "}\n";
+}
+
+const char* getFlatFragmentShader()
+{
+    return
+    "#version 330 core\n"
+
+    "in vec3 FragPos;\n"
+    "out vec4 FragColor;\n"
+
+    "uniform vec3 lightPos;\n"
+    "uniform vec3 lightColor;\n"
+    "uniform vec3 objectColor;\n"
+
+    "void main()\n"
+    "{\n"
+    "   vec3 dx = dFdx(FragPos);\n"
+    "   vec3 dy = dFdy(FragPos);\n"
+    "   vec3 normal = normalize(cross(dx, dy));\n"
+
+    "   vec3 lightDir = normalize(lightPos - FragPos);\n"
+    "   float diff = max(dot(normal, lightDir), 0.0);\n"
+
+    "   vec3 color = diff * lightColor * objectColor;\n"
+    "   FragColor = vec4(color, 1.0);\n"
+    "}\n";
+}
+
+// =============================== //
+// ========== MAIN RUN =========== //
+// =============================== //
+
+void A1solution::run(const std::string& filename) {
+
+    SceneData scene = loadSceneFromFile(filename);
+    GLFWwindow* window = initOpenGL(scene.width, scene.height);
+    if (!window) return;
+
+    // Compile and link shaders
+    // int shaderProgram = compileAndLinkShaders(getVertexShaderSource(), getFragmentShaderSource());
+    int phongShader = compileAndLinkShaders(getPhongVertexShader(), getPhongFragmentShader());
+    int flatShader = compileAndLinkShaders(getFlatVertexShader(), getFlatFragmentShader());
+    
+    int activeShader = phongShader;
+
+    // create mesh
+    Mesh mesh = createMesh(scene.vertices, scene.normals, scene.indices);
+
+    // debug_gl(0);
+
+    // =============================== //
+    //           SET UNIFORMS          //
+    // =============================== //
+    
+    glUseProgram(activeShader);
+    // GLint modelLoc = glGetUniformLocation(shaderProgram, "modelViewMatrix");
+    // GLint projLoc = glGetUniformLocation(shaderProgram, "projectionMatrix");
+    // glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &scene.modelViewMatrix[0][0]);
+    // glUniformMatrix4fv(projLoc, 1, GL_FALSE, &scene.projectionMatrix[0][0]);
+
+    // Phong uniforms
+    // glUniform3f(glGetUniformLocation(phongShader, "lightPos"), 5.0f, 5.0f, 5.0f);
+    // glUniform3f(glGetUniformLocation(phongShader, "viewPos"), 0.0f, 0.0f, 5.0f);
+    // glUniform3f(glGetUniformLocation(phongShader, "lightColor"), 1.0f, 1.0f, 1.0f);
+    // glUniform3f(glGetUniformLocation(phongShader, "objectColor"), 1.0f, 0.5f, 0.3f);
+
+    // =============================== //
+    //            MAIN LOOP            //
+    // =============================== //
+    while(!glfwWindowShouldClose(window))
+    {
+
+        // User input
+        if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+            activeShader = phongShader;
+        if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+            activeShader = flatShader;
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        // Each frame, reset color of each pixel to glClearColor
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(activeShader);
+
+        GLint modelLoc = glGetUniformLocation(activeShader, "modelViewMatrix");
+        GLint projLoc = glGetUniformLocation(activeShader, "projectionMatrix");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &scene.modelViewMatrix[0][0]);
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, &scene.projectionMatrix[0][0]);
+
+        // Lighting uniforms
+        glUniform3f(glGetUniformLocation(activeShader, "lightPos"), 5.0f, 5.0f, 5.0f);
+        glUniform3f(glGetUniformLocation(activeShader, "viewPos"), 0.0f, 0.0f, 5.0f);
+        glUniform3f(glGetUniformLocation(activeShader, "lightColor"), 1.0f, 1.0f, 1.0f);
+        glUniform3f(glGetUniformLocation(activeShader, "objectColor"), 1.0f, 0.5f, 0.3f);
+
+        glBindVertexArray(mesh.VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+        glDrawElements(GL_TRIANGLES, mesh.indexCount, GL_UNSIGNED_INT, 0);
+        glfwSwapBuffers(window);
+
+        // Detect inputs
+        glfwPollEvents();
+
+    }
+    // Shutdown GLFW
+    glfwTerminate();
+    return;
+
+}
