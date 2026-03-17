fa6ba0adf2b8d537a8559775b5e59e0b7b14e2b5
victori444 <victoriatthoang@gmail.com>
2026-03-17 14:35:13 -0400

    [A4] Fix multiple outputs

--- COMP371_RaytracerBase/code/src/RayTracer.cpp
+++ COMP371_RaytracerBase/code/src/RayTracer.cpp
@@ -98,136 +98,137 @@ void RayTracer::run() {
 
     // OUTPUT PARAMETERS //
 
-    auto output = sourceFile["output"][0];
+    for (auto& output : sourceFile["output"]) {
+        // image resolution in pixels
+        auto size = output["size"];
+        int width = size[0];
+        int height = size[1];
 
-    // image resolution in pixels
-    auto size = output["size"];
-    int width = size[0];
-    int height = size[1];
+        // camera
+        Eigen::Vector3d cameraCentre = jsonToVector(output["centre"]);
+        Eigen::Vector3d lookAt = jsonToVector(output["lookat"]);
+        Eigen::Vector3d up = jsonToVector(output["up"]);
+        float fov = output["fov"];
 
-    // camera
-    Eigen::Vector3d cameraCentre = jsonToVector(output["centre"]);
-    Eigen::Vector3d lookAt = jsonToVector(output["lookat"]);
-    Eigen::Vector3d up = jsonToVector(output["up"]);
-    float fov = output["fov"];
+        // Determine hit colour (default black, else white if black background)
+        Eigen::Vector3d backgroundColour = jsonToVector(output["bkc"]);
+        Eigen::Vector3d hitColour(0, 0, 0);
+        if (backgroundColour.norm() < 1e-6) {
+            hitColour = Eigen::Vector3d(1, 1, 1);
+        }
 
-    // Determine hit colour (default black, else white if black background)
-    Eigen::Vector3d backgroundColour = jsonToVector(output["bkc"]);
-    Eigen::Vector3d hitColour(0, 0, 0);
-    if (backgroundColour.norm() < 1e-6) {
-        hitColour = Eigen::Vector3d(1, 1, 1);
-    }
+        // camera coordinate system
+        Eigen::Vector3d forward = lookAt.normalized(); // direction camera looks at
+        Eigen::Vector3d right = forward.cross(up).normalized(); // perpendicular to forward & up
+        Eigen::Vector3d trueUp = right.cross(forward).normalized(); // vertical axis
 
-    // camera coordinate system
-    Eigen::Vector3d forward = lookAt.normalized(); // direction camera looks at
-    Eigen::Vector3d right = forward.cross(up).normalized(); // perpendicular to forward & up
-    Eigen::Vector3d trueUp = right.cross(forward).normalized(); // vertical axis
+        // image plane dimensions
+        double fovRadians = fov * M_PI / 180.0; // convert FOV from degrees to radians
+        double planeHeight = 2.0 * tan(fovRadians / 2.0); // Plane height wrt vertical FOV
+        double aspectRatio = (double)width / height;
+        double planeWidth = aspectRatio * planeHeight;
 
-    // image plane dimensions
-    double fovRadians = fov * M_PI / 180.0; // convert FOV from degrees to radians
-    double planeHeight = 2.0 * tan(fovRadians / 2.0); // Plane height wrt vertical FOV
-    double aspectRatio = (double)width / height;
-    double planeWidth = aspectRatio * planeHeight;
+        // frame buffer [r,g,b,r,g,b,...]
+        std::vector<double> framebuffer(3 * width * height);
 
-    // frame buffer [r,g,b,r,g,b,...]
-    std::vector<double> framebuffer(3 * width * height);
+        // ray generation loop
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
 
-    // ray generation loop
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
+                // convert px coordinates to NDC [0,1]
+                double u = (x + 0.5) / width;
+                double v = (y + 0.5) / height;
 
-            // convert px coordinates to NDC [0,1]
-            double u = (x + 0.5) / width;
-            double v = (y + 0.5) / height;
+                // NDC to screen space [-1, 1] wrt image plane
+                double screenX = (2 * u - 1) * planeWidth / 2;
+                double screenY = (1 - 2 * v) * planeHeight / 2; // inverts y for img coordinates
 
-            // NDC to screen space [-1, 1] wrt image plane
-            double screenX = (2 * u - 1) * planeWidth / 2;
-            double screenY = (1 - 2 * v) * planeHeight / 2; // inverts y for img coordinates
+                // determine direction of ray from camera through pixel on img plane, construct ray
+                Eigen::Vector3d direction = (forward + (screenX * right) + (screenY * trueUp)).normalized();
+                Ray ray(direction, cameraCentre);
 
-            // determine direction of ray from camera through pixel on img plane, construct ray
-            Eigen::Vector3d direction = (forward + (screenX * right) + (screenY * trueUp)).normalized();
-            Ray ray(direction, cameraCentre);
+                // Check if intersection exists at pixel
+                Eigen::Vector3d pixelColour = backgroundColour;
+                double EPS = 1e-6;
+                double closestT = 1e9;
+                bool hit = false;
+                Geometry* hitObject = nullptr;
 
-            // Check if intersection exists at pixel
-            Eigen::Vector3d pixelColour = backgroundColour;
-            double EPS = 1e-6;
-            double closestT = 1e9;
-            bool hit = false;
-            Geometry* hitObject = nullptr;
-
-            // object intersection
-            for (auto& obj : objects) {
-                double t;
-                if (obj->tryIntersectRay(ray, t)) {
-                    if (t > EPS && t < closestT) {
-                        closestT = t;
-                        // pixelColour = hitColour; // set colour if intersection
-                        hitObject = obj.get();
-                        hit = true;
-                    }
-                }
-            } 
-
-            // final pixel colour
-            if (hit) {
-
-                Eigen::Vector3d hitPoint = cameraCentre + closestT * direction;
-                Eigen::Vector3d N = hitObject->getNormal(hitPoint).normalized();
-                Eigen::Vector3d V = (cameraCentre - hitPoint).normalized();
-                Eigen::Vector3d colour(0,0,0);
-
-                // AMBIENT LIGHT
-                Eigen::Vector3d ambientIntensity = jsonToVector(output["ai"]);
-                colour += hitObject->ka * hitObject->ac.cwiseProduct(ambientIntensity);
-
-                // loop through lights
-                for (auto& light : lights) {
-                    Eigen::Vector3d L = (light.position - hitPoint).normalized();
-                    double lightDistance = (light.position - hitPoint).norm();
-
-                    // SHADOW
-                    Ray shadowRay(L, hitPoint + EPS * N);
-                    bool inShadow = false;
-                    
-                    for (auto& obj : objects) {
-                        double t;
-                        if (obj->tryIntersectRay(shadowRay, t)) {
-                            if (t > EPS && t < lightDistance) {
-                                inShadow = true;
-                                break;
-                            }
+                // object intersection
+                for (auto& obj : objects) {
+                    double t;
+                    if (obj->tryIntersectRay(ray, t)) {
+                        if (t > EPS && t < closestT) {
+                            closestT = t;
+                            // pixelColour = hitColour; // set colour if intersection
+                            hitObject = obj.get();
+                            hit = true;
                         }
                     }
+                } 
 
-                    if (inShadow) continue;
+                // final pixel colour
+                if (hit) {
 
-                    // DIFFUSE LIGHT
+                    Eigen::Vector3d hitPoint = cameraCentre + closestT * direction;
+                    Eigen::Vector3d N = hitObject->getNormal(hitPoint).normalized();
+                    Eigen::Vector3d V = (cameraCentre - hitPoint).normalized();
+                    Eigen::Vector3d colour(0,0,0);
 
-                    bool twoSidedRendering = output.value("twosiderender", true);
-                    double NdotL = twoSidedRendering ? std::abs(N.dot(L)) : std::max(0.0, N.dot(L));
-                    Eigen::Vector3d diffuse = hitObject->kd * NdotL * hitObject->dc.cwiseProduct(light.id);
+                    // AMBIENT LIGHT
+                    Eigen::Vector3d ambientIntensity = jsonToVector(output["ai"]);
+                    colour += hitObject->ka * hitObject->ac.cwiseProduct(ambientIntensity);
 
-                    // SPECULAR
+                    // loop through lights
+                    for (auto& light : lights) {
+                        Eigen::Vector3d L = (light.position - hitPoint).normalized();
+                        double lightDistance = (light.position - hitPoint).norm();
 
-                    Eigen::Vector3d H = (L + V).normalized();
-                    double NdotH = std::max(0.0, N.dot(H));
-                    Eigen::Vector3d specular = hitObject->ks * pow(NdotH, hitObject->pc) * hitObject->sc.cwiseProduct(light.is);
-                    colour += diffuse + specular;
+                        // SHADOW
+                        Ray shadowRay(L, hitPoint + EPS * N);
+                        bool inShadow = false;
+                        
+                        for (auto& obj : objects) {
+                            double t;
+                            if (obj->tryIntersectRay(shadowRay, t)) {
+                                if (t > EPS && t < lightDistance) {
+                                    inShadow = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (inShadow) continue;
+
+                        // DIFFUSE LIGHT
+
+                        bool twoSidedRendering = output.value("twosiderender", true);
+                        double NdotL = twoSidedRendering ? std::abs(N.dot(L)) : std::max(0.0, N.dot(L));
+                        Eigen::Vector3d diffuse = hitObject->kd * NdotL * hitObject->dc.cwiseProduct(light.id);
+
+                        // SPECULAR
+
+                        Eigen::Vector3d H = (L + V).normalized();
+                        double NdotH = std::max(0.0, N.dot(H));
+                        Eigen::Vector3d specular = hitObject->ks * pow(NdotH, hitObject->pc) * hitObject->sc.cwiseProduct(light.is);
+                        colour += diffuse + specular;
+                    }
+
+                    pixelColour = colour;
+                    pixelColour = pixelColour.cwiseMax(0.0).cwiseMin(1.0);
                 }
 
-                pixelColour = colour;
-                pixelColour = pixelColour.cwiseMax(0.0).cwiseMin(1.0);
+                // store pixels in framebuffer
+                int index = 3 * (y * width + x);
+                framebuffer[index + 0] = pixelColour[0];
+                framebuffer[index + 1] = pixelColour[1];
+                framebuffer[index + 2] = pixelColour[2];
             }
-
-            // store pixels in framebuffer
-            int index = 3 * (y * width + x);
-            framebuffer[index + 0] = pixelColour[0];
-            framebuffer[index + 1] = pixelColour[1];
-            framebuffer[index + 2] = pixelColour[2];
         }
-    }
 
     // save final image
     std::string filename = output["filename"];
     save_ppm(filename, framebuffer, width, height);
+
+    }
 }
